df67ed3feff64eadcee143c892b59fedba71fb25
Oleg Pavlyuk <pavlyuk.o.yu@gmail.com>
2019-09-21 12:15:57 +0300

    add simple router to the server

--- server.js
+++ server.js
@@ -27,9 +27,58 @@ const server = http.createServer((req, res) => {
   req.on('end', () => {
     buffer += decoder.end();
 
-    // Send response
-    res.end('Hello from server');
+    // Choose the handler this request should go to, otherwise use notFound handler
+    const chosenHandler =
+      typeof router[trimmedPath] !== 'undefined'
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // Construct data object from the request
+    const data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer
+    };
+
+    chosenHandler(data, function(statusCode, payload) {
+      // Use handler's status code or default to 200
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+
+      // Use handler's payload or default to {}
+      payload = typeof payload === 'object' ? payload : {};
+
+      // Convert payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Send response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log(`Returning this respone: ${statusCode}, ${payloadString}`);
+    });
   });
 });
 
 server.listen(3000, () => console.log(`Server listening on port 3000`));
+
+// Define handlers
+const handlers = {};
+
+// Define test handler
+handlers.test = (data, callback) => {
+  // Callback http status code and payload object
+  callback(406, { name: 'test handler' });
+};
+
+// Define not found handler
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define router
+const router = {
+  test: handlers.test
+};
